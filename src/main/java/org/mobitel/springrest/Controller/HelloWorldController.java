0073b18e2ed9f0959cc442f9a81e7739ff37a60d
malithka <malithka@mobitel.lk>
2024-05-26 22:54:37 +0530

    adding i18n into application using messageSource

--- src/main/java/org/mobitel/springrest/Controller/HelloWorldController.java
+++ src/main/java/org/mobitel/springrest/Controller/HelloWorldController.java
@@ -1,11 +1,21 @@
 package org.mobitel.springrest.Controller;
 
 import org.mobitel.springrest.Bean.HelloWorldBean;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 @RequestMapping("api/v1")
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+    public HelloWorldController(MessageSource messageSource){
+        this.messageSource= messageSource;
+
+    }
 
     @RequestMapping(method = RequestMethod.GET, path = "/hello")
     public String sayHello(){
@@ -24,5 +34,9 @@ public class HelloWorldController {
             return new HelloWorldBean("Hello World " +name);
     }
 
-
+    @RequestMapping(method = RequestMethod.GET, path = "/sayGoodMorning")
+    public String sayGoodMorning(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return this.messageSource.getMessage("good.morning.message" , null, "Default Message", locale);
+    }
 }

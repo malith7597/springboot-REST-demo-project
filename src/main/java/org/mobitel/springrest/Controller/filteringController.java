76b20931bb287fbcc6fe9cda85e337fd7e4e1670
malithka <malithka@mobitel.lk>
2024-05-28 23:00:37 +0530

    adding Static Filtering using JsonIgnore and JsonIgnoreProperties annotations

--- src/main/java/org/mobitel/springrest/Controller/filteringController.java
+++ src/main/java/org/mobitel/springrest/Controller/filteringController.java
@@ -1,2 +1,22 @@
-package org.mobitel.springrest.Controller;public class filteringController {
-}
+package org.mobitel.springrest.Controller;
+
+import org.mobitel.springrest.Bean.someBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class filteringController {
+    @GetMapping("/filtering")
+    public someBean returnFilterBean(){
+        return new someBean();
+    }
+
+    @GetMapping("/filtering-list")
+    public List<someBean> filterList(){
+        return Arrays.asList(new someBean("field2" ,"field3","field4"), new someBean("field6","field7","field8"));
+    }
+ }
